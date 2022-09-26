a616321bb293f37acaa0958639d50f86f449b8f5
Daffominga <45728697+Daffominga@users.noreply.github.com>
2022-09-26 15:03:37 +0800

    Update train.py

--- train.py
+++ train.py
@@ -4,16 +4,18 @@ import torch
 import torch.nn.functional as F
 from torch_geometric.datasets import Planetoid
 import torch_geometric.transforms as T
+from torch_geometric.utils import get_laplacian, add_self_loops
+from torch_geometric.nn.conv import MessagePassing
 from layers import resGCNConv
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--layer', type=int, default=64, help='Number of layers.')
+parser.add_argument('--layer', type=int, default=32, help='Number of layers.')
 args = parser.parse_args()
 
-dataset = 'Cora'  # 64
+# dataset = 'Cora'  # 64
 # dataset = 'CiteSeer'  # 32
-# dataset = 'PubMed'  # 64
+dataset = 'PubMed'  # 32
 path = osp.join(osp.dirname(osp.realpath(__file__)), '.', 'data', dataset)
 dataset = Planetoid(path, dataset, split="public", transform=T.NormalizeFeatures())
 data = dataset[0]
@@ -28,38 +30,47 @@ dropout = 0.6
 alpha = 0.9
 gamma = 0.1
 lamda = 0.5
-hidden_dim = 64
-weight_decay1 = 0.01
+hidden_dim = 128
+weight_decay1 = 0.001
 weight_decay2 = 5e-4
 lr = 0.01
-patience = 100
+patience = 50
 #####################
 
 GConv = resGCNConv
+mp = MessagePassing()
 
 
 class resGCNmodel(torch.nn.Module):
     def __init__(self):
         super(resGCNmodel, self).__init__()
+        self.hpconvs = torch.nn.Linear(dataset.num_features, hidden_dim)
         self.convs = torch.nn.ModuleList()
         self.convs.append(torch.nn.Linear(dataset.num_features, hidden_dim))
         for _ in range(nlayer):
             self.convs.append(GConv(hidden_dim, hidden_dim))
         self.convs.append(torch.nn.Linear(hidden_dim, dataset.num_classes))
-        self.reg_params = list(self.convs[1:-1].parameters())
-        self.non_reg_params = list(self.convs[0:1].parameters())+list(self.convs[-1:].parameters())
+        self.reg_params = list(self.convs[0:1].parameters()) + list(self.convs[1:-1].parameters()) \
+                          + list(self.hpconvs.parameters())
+        self.non_reg_params = list(self.convs[-1:].parameters())
+
 
     def forward(self):
         x, edge_index, edge_weight = data.x, data.edge_index, data.edge_attr
         _hidden = []
         x = F.dropout(x, dropout, training=self.training)
         x = F.relu(self.convs[0](x))
+
+        self_edge_index, self_edge_weight = add_self_loops(edge_index, edge_weight)
+        lap_edge_index, lap_edge_weight = get_laplacian(self_edge_index, self_edge_weight)
+        lx = mp.propagate(lap_edge_index, x=data.x, edge_weight=lap_edge_weight)  # equals to matmul(Laplacian, feature_X)
+        lx = F.relu(self.hpconvs(lx))
+
         _hidden.append(x)
         for i, con in enumerate(self.convs[1: -1]):  # such as (1, 'conv_i')
             x = F.dropout(x, dropout, training=self.training)
             beta = lamda / ((i + 1) + 1)
-            # beta = math.log(lamda/(i+1)+1)
-            x = F.relu(con(x, edge_index, alpha, gamma, _hidden[0], beta, edge_weight))
+            x = F.relu(con(x, edge_index, alpha, gamma, _hidden[0], beta, lx, edge_weight))
         x = F.dropout(x, dropout, training=self.training)
         x = self.convs[-1](x)  # self.convs.append(torch.nn.Linear(hidden_dim, dataset.num_classes))
         return F.log_softmax(x, dim=1)
@@ -113,11 +124,6 @@ for epoch in range(1, 1000):
         print(log.format(epoch, loss_tra, loss_val, test_acc))
 
     if bad_counter == patience:
-        # w = model.convs[1].weight1
-        # res = w.cpu().detach().numpy()
-        # res = np.array(res)
-        # np.savetxt('npresult1.txt', res)
-        # io.savemat('save.mat', {'result1': res})
         break
 
 log = 'best Epoch: {:03d}, Val loss: {:.4f}, Test acc: {:.4f}'
